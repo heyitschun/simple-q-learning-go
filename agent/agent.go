2caa668c7ff348e4fb0ae1adffd6ce44bdd37846
heyitschun <cpoon19@gmail.com>
2020-11-10 18:34:05 +0100

    NewEpsilon and Bet

--- agent/agent.go
+++ agent/agent.go
@@ -1,5 +1,7 @@
 package agent
 
+import "math/rand"
+
 type Agent struct {
 	Epsilon        float64
 	Alpha          float64
@@ -11,4 +13,15 @@ type Agent struct {
 func (a *Agent) NewEpsilon() float64 {
 	var alpha = 1.0 - a.Alpha
 	return alpha * a.Epsilon
+}
+
+func (a *Agent) Bet(nTrial int) bool {
+	if nTrial <= a.MinExplore {
+		if rand.Float64() > 0.5 {
+			return true
+		}
+		return false
+	} else if rand.Float64() <= a.Epsilon {
+		a.Epsilon = a.NewEpsilon()
+	}
 }
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	n := rand.Float64()
+	fmt.Println(n)
+}
